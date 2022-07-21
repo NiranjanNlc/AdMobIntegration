4f08671de19adbf64115d82f7e84affc7684f003
niranjannlc <niranjannlc10@gmail.com>
2022-07-21 11:59:31 +0545

    initalize mobads on maina ctivity

--- app/src/main/java/com/example/admobpractise/MainActivity.kt
+++ app/src/main/java/com/example/admobpractise/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.admobpractise
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.ads.MobileAds
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        MobileAds.initialize(this) {}
+
     }
 }
