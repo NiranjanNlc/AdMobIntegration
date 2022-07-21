10b925a0fd6295583070085fd919701efa6ac400
niranjannlc <niranjannlc10@gmail.com>
2022-07-21 12:11:22 +0545

    loaded adview

--- app/src/main/java/com/example/admobpractise/MainActivity.kt
+++ app/src/main/java/com/example/admobpractise/MainActivity.kt
@@ -2,13 +2,18 @@ package com.example.admobpractise
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.admobpractise.databinding.ActivityMainBinding
+import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.MobileAds
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
         MobileAds.initialize(this) {}
-
+        val adRequest = AdRequest.Builder().build()
+        binding.adView.loadAd(adRequest)
     }
 }
